0932e19339748cd01141c1c60eceff8bc5c35c63
Ariful Islam <coder.arifbd@gmail.com>
2021-02-08 15:30:46 +0600

    final code submit

--- app.js
+++ app.js
@@ -1,32 +1,62 @@
-document.getElementById("country-search").addEventListener("click", function () {
-    const country = document.getElementById("country").value
-    getFoodList(country)
+document.getElementById("submit").addEventListener("click", function () {
+    const area = document.getElementById("area").value
+    getAreaWiseFoodList(area)
 })
 
-function getFoodList(country) {
-    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a='+country+'')
+
+function getAreaWiseFoodList(area) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`)
         .then(res => res.json())
-        .then(data => getMealList(data))
+        .then(data => displayFood(data.meals))
 
-    const getMealList = meals => {
-        const mealList = meals.meals
-        const foodList = document.getElementById("food-list")
-        for (let i = 0; i < mealList.length; i++) {
-            const food = mealList[i]
-            const foodName = mealList[i].strMeal
-            const foodImg = mealList[i].strMealThumb
+    function displayFood(foods) {
+        // console.log(foods)
+        const ul = document.getElementById("food-list")
+        for (let i = 0; i < foods.length; i++) {
+            const food = foods[i];
             const li = document.createElement("li")
-            const img = document.createElement("img")
-            img.setAttribute("src", `${foodImg}`)
-            const h3 = document.createElement("h3")
-            h3.innerText = foodName
-            li.appendChild(img)
-            li.appendChild(h3)
-            foodList.appendChild(li)
-
-            // console.log(foodName)
+            li.innerHTML = `
+            <button id="food-details" onclick="foodDetails(${food.idMeal})">
+                <img class="food-image" src="${food.strMealThumb}">
+                <h1 class="food-name">${food.strMeal}</h1>
+            </button>
+        `
+            ul.appendChild(li)
+            // console.log(food)
         }
-        // console.log(mealList[1]);
+    }
+}
+const foodDetails = id => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then(res => res.json())
+        .then(data => singleDetails(data.meals[0]))
 
+    function singleDetails(name) {
+        const imgUrl = name.strMealThumb
+        const foodName = name.strMeal
+        const ingredients = [name.strIngredient1, name.strIngredient2, name.strIngredient3, name.strIngredient4, name.strIngredient5, name.strIngredient6, name.strIngredient7, name.strIngredient8, name.strIngredient9, name.strIngredient10, name.strIngredient11, name.strIngredient12, name.strIngredient13, name.strIngredient14, name.strIngredient15, name.strIngredient16, name.strIngredient17, name.strIngredient18, name.strIngredient19, name.strIngredient20]
+
+        const singleDetailsContainer = document.getElementById("single-food-details")
+        singleDetailsContainer.innerHTML = `
+            <span id="hide">Close</span>
+            <img src="${imgUrl}">
+            <h1>${foodName}</h1>
+            <ul id="ingredients-list"></ul>
+        `
+        const ingredientsList = document.getElementById("ingredients-list")
+        for (let i = 0; i < ingredients.length; i++) {
+            const ingredientsItem = ingredients[i];
+            const li = document.createElement("li")
+            if (ingredientsItem == '') {
+                continue;
+                3
+            }
+            li.innerText = ingredientsItem
+            ingredientsList.appendChild(li)
+        }
+        document.getElementById("single-food-details").style.display = "block"
+        document.getElementById("hide").addEventListener("click", function () {
+            document.getElementById("single-food-details").style.display = "none"
+        })
     }
 }
